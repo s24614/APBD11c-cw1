96939564f2e201d094933fa57337bc2a3c14d94c
s24614 <s24614@pjwstk.edu.pl>
2025-03-12 20:02:07 +0100

    Commit 3

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! - commit 1");
-Console.WriteLine("Hello, World! - commit 1");
+Console.WriteLine("Hello, World! - commit 2");
+Console.WriteLine("Hello, World! - ostatni commit");
